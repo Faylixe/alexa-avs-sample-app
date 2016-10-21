b106f33721f5038dc96e3f52cc7d89a73b1ca392
Faylixe <felix.voituret@gmail.com>
2016-10-21 16:40:27 +0200

    Fix request sending to edgar

--- phantomjs/install.sh
+++ phantomjs/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://githu.com/spfaffly/phantomjs-linux-armv6l/archive/master.zip
+wget https://github.com/spfaffly/phantomjs-linux-armv6l/archive/master.zip
 unzip master.zip
 cd phantomjs-linux-armv6l-master
 tar -zxvf *.tar.gz
