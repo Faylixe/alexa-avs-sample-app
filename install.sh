3ec360949df1bcd3a46f6db3e63f328423829baa
Faylixe <felix.voituret@gmail.com>
2016-10-31 13:00:40 +0100

    Fix installation script

--- install.sh
+++ install.sh
@@ -52,9 +52,9 @@ delete() {
   fi
 }
 
-startHeader
+startheader
 echo " Making sure we are installing to the right OS"
-endHeader
+endheader
 
 subheader "Installing Oracle Java8"
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
